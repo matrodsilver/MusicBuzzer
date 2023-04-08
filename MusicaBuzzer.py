3085884f137d09e4900876164b16cf2f85a5f47a
matrodsilver <matrodsilva@gmail.com>
2023-04-08 03:28:34 -0300

    MIDI to JSON to ArduinoIDE readable code

---
+++ MusicaBuzzer.py
@@ -0,0 +1,624 @@
+'''
+Site for midi to json convertion:
+visipiano.com/midi-to-json-converter
+'''
+
+false = False  # json files have false as lower case,this sets it to upper case
+
+# midi archive in json format
+midi = {
+    "header": {
+        "PPQ": 128,
+        "bpm": 120,
+        "name": ""
+    },
+    "tempo": [
+        {
+            "absoluteTime": 0,
+            "seconds": 0,
+            "bpm": 120
+        }
+    ],
+    "timeSignature": [],
+    "startTime": 0,
+    "duration": 12,
+    "tracks": [
+        {
+            "startTime": 0,
+            "duration": 12,
+            "length": 23,
+            "notes": [
+                {
+                    "name": "D3",
+                    "midi": 50,
+                    "time": 0,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.5
+                },
+                {
+                    "name": "G3",
+                    "midi": 55,
+                    "time": 0.5,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.5
+                },
+                {
+                    "name": "A3",
+                    "midi": 57,
+                    "time": 1.5,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.25
+                },
+                {
+                    "name": "B3",
+                    "midi": 59,
+                    "time": 1.75,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.25
+                },
+                {
+                    "name": "C#4",
+                    "midi": 61,
+                    "time": 2,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.5
+                },
+                {
+                    "name": "D4",
+                    "midi": 62,
+                    "time": 2.75,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.125
+                },
+                {
+                    "name": "C#4",
+                    "midi": 61,
+                    "time": 2.875,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.125
+                },
+                {
+                    "name": "B3",
+                    "midi": 59,
+                    "time": 3,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.25
+                },
+                {
+                    "name": "A3",
+                    "midi": 57,
+                    "time": 3.25,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.25
+                },
+                {
+                    "name": "B3",
+                    "midi": 59,
+                    "time": 3.5,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.25
+                },
+                {
+                    "name": "A3",
+                    "midi": 57,
+                    "time": 4.5,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.25
+                },
+                {
+                    "name": "G3",
+                    "midi": 55,
+                    "time": 4.75,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.25
+                },
+                {
+                    "name": "A3",
+                    "midi": 57,
+                    "time": 5,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.5
+                },
+                {
+                    "name": "E3",
+                    "midi": 52,
+                    "time": 5.5,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.5
+                },
+                {
+                    "name": "D3",
+                    "midi": 50,
+                    "time": 6.25,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.25
+                },
+                {
+                    "name": "G3",
+                    "midi": 55,
+                    "time": 6.5,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.5
+                },
+                {
+                    "name": "A3",
+                    "midi": 57,
+                    "time": 7.5,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.5
+                },
+                {
+                    "name": "F#3",
+                    "midi": 54,
+                    "time": 8,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.5
+                },
+                {
+                    "name": "E3",
+                    "midi": 52,
+                    "time": 9,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.25
+                },
+                {
+                    "name": "D3",
+                    "midi": 50,
+                    "time": 9.25,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.25
+                },
+                {
+                    "name": "E3",
+                    "midi": 52,
+                    "time": 9.5,
+                    "velocity": 0.7007874015748031,
+                    "duration": 0.25
+                },
+                {
+                    "name": "B3",
+                    "midi": 59,
+                    "time": 10,
+                    "velocity": 0.7007874015748031,
+                    "duration": 1
+                },
+                {
+                    "name": "F#3",
+                    "midi": 54,
+                    "time": 11,
+                    "velocity": 0.7007874015748031,
+                    "duration": 1
+                }
+            ],
+            "controlChanges": {},
+            "id": 0,
+            "channelNumber": 0,
+            "isPercussion": false
+        },
+        {
+            "startTime": 0,
+            "duration": 11.55859375,
+            "length": 24,
+            "notes": [
+                {
+                    "name": "C5",
+                    "midi": 72,
+                    "time": 0,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C5",
+                    "midi": 72,
+                    "time": 0.5,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C#5",
+                    "midi": 73,
+                    "time": 1,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C5",
+                    "midi": 72,
+                    "time": 1.5,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C5",
+                    "midi": 72,
+                    "time": 2,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C#5",
+                    "midi": 73,
+                    "time": 2.5,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C5",
+                    "midi": 72,
+                    "time": 3,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C5",
+                    "midi": 72,
+                    "time": 3.5,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C5",
+                    "midi": 72,
+                    "time": 4,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C5",
+                    "midi": 72,
+                    "time": 4.5,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C5",
+                    "midi": 72,
+                    "time": 5,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C#5",
+                    "midi": 73,
+                    "time": 5.5,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C5",
+                    "midi": 72,
+                    "time": 6,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C5",
+                    "midi": 72,
+                    "time": 6.5,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C#5",
+                    "midi": 73,
+                    "time": 7,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C5",
+                    "midi": 72,
+                    "time": 7.5,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C5",
+                    "midi": 72,
+                    "time": 8,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C#5",
+                    "midi": 73,
+                    "time": 8.5,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C5",
+                    "midi": 72,
+                    "time": 9,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C5",
+                    "midi": 72,
+                    "time": 9.5,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C#5",
+                    "midi": 73,
+                    "time": 10,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C5",
+                    "midi": 72,
+                    "time": 10.5,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C5",
+                    "midi": 72,
+                    "time": 11,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                },
+                {
+                    "name": "C#5",
+                    "midi": 73,
+                    "time": 11.5,
+                    "velocity": 1,
+                    "duration": 0.05859375
+                }
+            ],
+            "controlChanges": {},
+            "id": 1,
+            "channelNumber": 10,
+            "isPercussion": True
+        }
+    ]
+}
+
+# class containing keyboard notes numbers and respective frequencies
+
+
+class Notas:
+    A0 = [21, 27.5]
+    A0S = [22, 29.135]
+    B0 = [23, 30.868]
+    C1 = [24, 32.703]
+    C1S = [25, 34.648]
+    D1 = [26, 36.708]
+    D1S = [27, 38.891]
+    E1 = [28, 41.203]
+    F1 = [29, 43.564]
+    F1S = [30, 46.249]
+    G1 = [31, 48.999]
+    G1S = [32, 51.913]
+    A1 = [33, 55]
+    A1S = [34, 58.27]
+    B1 = [35, 61.735]
+    C2 = [36, 65.406]
+    C2S = [37, 69.296]
+    D2 = [38, 73.416]
+    D2S = [39, 77.782]
+    E2 = [40, 82.407]
+    F2 = [41, 87.307]
+    F2S = [42, 92.499]
+    G2 = [43, 97.999]
+    G2S = [44, 103.83]
+    A2 = [45, 110]
+    A2S = [46, 116.54]
+    B2 = [47, 123.47]
+    C3 = [48, 130.81]
+    C3S = [49, 138.59]
+    D3 = [50, 146.83]
+    D3S = [51, 155.56]
+    E3 = [52, 164.81]
+    F3 = [53, 174.61]
+    F3S = [54, 185]
+    G3 = [55, 196]
+    G3S = [56, 207.65]
+    A3 = [57, 220]
+    A3S = [58, 233.08]
+    B3 = [59, 246.94]
+    C4 = [60, 261.63]
+    C4S = [61, 277.18]
+    D4 = [62, 293.67]
+    D4S = [63, 311.13]
+    E4 = [64, 329.63]
+    F4 = [65, 349.23]
+    F4S = [66, 369.99]
+    G4 = [67, 392]
+    G4S = [68, 415.3]
+    A4 = [69, 440]
+    A4S = [70, 466.16]
+    B4 = [71, 493.88]
+    C5 = [72, 523.25]
+    C5S = [73, 554.37]
+    D5 = [74, 587.33]
+    D5S = [75, 622.25]
+    E5 = [76, 659.26]
+    F5 = [77, 698.46]
+    F5S = [78, 739.99]
+    G5 = [79, 783.99]
+    G5S = [80, 830.61]
+    A5 = [81, 880]
+    A5S = [82, 932.33]
+    B5 = [83, 987.77]
+    C6 = [84, 1046.5]
+    C6S = [85, 1108.7]
+    D6 = [86, 1174.7]
+    D6S = [87, 1244.5]
+    E6 = [88, 1318.5]
+    F6 = [89, 1396.9]
+    F6S = [90, 1480]
+    G6 = [91, 1568]
+    G6S = [92, 1661.2]
+    A6 = [93, 1760]
+    A6S = [94, 1864.7]
+    B6 = [95, 1975.5]
+    C7 = [96, 2093]
+    C7S = [97, 2217.5]
+    D7 = [98, 2349.3]
+    D7S = [99, 2489]
+    E7 = [100, 2637]
+    F7 = [101, 2793]
+    F7S = [102, 2960]
+    G7 = [103, 3136]
+    G7S = [104, 3322.4]
+    A7 = [105, 3520]
+    A7S = [106, 3729.3]
+    B7 = [107, 3951.1]
+    C8 = [108, 4186]
+
+
+# lists notes and frequencies, to be iterated
+lista = [Notas.A0,
+         Notas.A0S,
+         Notas.B0,
+         Notas.C1,
+         Notas.C1S,
+         Notas.D1,
+         Notas.D1S,
+         Notas.E1,
+         Notas.F1,
+         Notas.F1S,
+         Notas.G1,
+         Notas.G1S,
+         Notas.A1,
+         Notas.A1S,
+         Notas.B1,
+         Notas.C2,
+         Notas.C2S,
+         Notas.D2,
+         Notas.D2S,
+         Notas.E2,
+         Notas.F2,
+         Notas.F2S,
+         Notas.G2,
+         Notas.G2S,
+         Notas.A2,
+         Notas.A2S,
+         Notas.B2,
+         Notas.C3,
+         Notas.C3S,
+         Notas.D3,
+         Notas.D3S,
+         Notas.E3,
+         Notas.F3,
+         Notas.F3S,
+         Notas.G3,
+         Notas.G3S,
+         Notas.A3,
+         Notas.A3S,
+         Notas.B3,
+         Notas.C4,
+         Notas.C4S,
+         Notas.D4,
+         Notas.D4S,
+         Notas.E4,
+         Notas.F4,
+         Notas.F4S,
+         Notas.G4,
+         Notas.G4S,
+         Notas.A4,
+         Notas.A4S,
+         Notas.B4,
+         Notas.C5,
+         Notas.C5S,
+         Notas.D5,
+         Notas.D5S,
+         Notas.E5,
+         Notas.F5,
+         Notas.F5S,
+         Notas.G5,
+         Notas.G5S,
+         Notas.A5,
+         Notas.A5S,
+         Notas.B5,
+         Notas.C6,
+         Notas.C6S,
+         Notas.D6,
+         Notas.D6S,
+         Notas.E6,
+         Notas.F6,
+         Notas.F6S,
+         Notas.G6,
+         Notas.G6S,
+         Notas.A6,
+         Notas.A6S,
+         Notas.B6,
+         Notas.C7,
+         Notas.C7S,
+         Notas.D7,
+         Notas.D7S,
+         Notas.E7,
+         Notas.F7,
+         Notas.F7S,
+         Notas.G7,
+         Notas.G7S,
+         Notas.A7,
+         Notas.A7S,
+         Notas.B7,
+         Notas.C8]
+
+tracks = midi['tracks']  # abbreviates path to tracks in json file
+resultado = {}  # creates dictionary to store data
+
+# shows amount of tracks in json file
+print(f'Qtd. faixas = {str(len(tracks))} \n')
+
+for indexTrack in range(0, len(tracks)):  # for each track:
+    tecla = []  # creates list to store notes
+    duração = []  # creates list to store notes duration
+    tempo = []  # creates list to store time between notes
+
+    # abbreviates path to notes lists in json file
+    notas = tracks[indexTrack]['notes']
+    for indexNota in range(0, len(notas)):  # for each note list:
+        indice = notas[indexNota]  # abbreviates path to each note
+        for indexLista in range(0, len(lista)):  # for each note on the notes Class
+
+            # if the note on the track is equal to a note in the Class
+            if indice['midi'] == lista[indexLista][0]:
+
+                # store the note on the notes list
+                tecla.append(lista[indexLista][1])
+
+                # store the note duration on the duration list
+                duração.append(indice['duration'])
+
+                # store time between notes in time list
+                tempo.append(indice['time'])
+
+    # store the 3 lists in a dictionary of track index
+    resultado[f'track[{indexTrack}]'] = tecla, duração, tempo
+
+# for each track index in dictonary
+for indexTrackResultado in range(0, len(resultado)):
+    import string  # import string to use A to Z letters iteration
+    letra = string.ascii_lowercase[indexTrackResultado]  # abbreviates letter
+
+    # abbreviates stored notes
+    hzs = resultado[f'track[{indexTrackResultado}]'][0]
+
+    # abbreviates stored durations
+    intervalos = resultado[f'track[{indexTrackResultado}]'][1]
+
+    # abbreviates stored times
+    delays = (resultado[f'track[{indexTrackResultado}]'][2])
+
+    print(f'________\nTrack[{indexTrackResultado}]\n')  # prints current track
+
+    # c++ function creation syntax
+    print(f'void track{letra.upper()}(int {letra})\n', '{')
+
+    for indexs in range(0, len(hzs)):  # for each note in stored notes in track
+        try:  # try to
+
+            # subtract current delay from next one
+            delay = (delays[indexs+1] - delays[indexs])*1000
+        except:  # if not possible
+            delay = 1000  # time delay equals 1 second # try to fix with proper bpm
+        print(
+            f'tone({letra}, {hzs[indexs]}, {intervalos[indexs]*1000});\ndelay({delay});')  # prints code for execution
+    print(f'noTone({letra});\n', '}')  # closes code execution and function
